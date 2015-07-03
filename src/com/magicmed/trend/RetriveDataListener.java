0b5da174b159f98f6dc8a438c07cf9f511f94031
sunshine <sunshine46582747@sina.com>
2015-07-03 20:45:00 +0800

    Add callback interface for getDataFromDB() function.

---
+++ src/com/magicmed/trend/RetriveDataListener.java
@@ -0,0 +1,11 @@
+package com.magicmed.trend;
+
+/**
+ * {@link DatabaseConverter}.foo() Callback listener.
+ * @author EXLsunshine
+ *
+ */
+public interface RetriveDataListener 
+{
+	public void onFinishRetrivingData(String filePath);
+}
